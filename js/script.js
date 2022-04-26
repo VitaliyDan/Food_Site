5671387afca7766cf53ac486cda5cf8366c3b664
Vitaliy Danaykanich <vitailyc.ua@gmail.com>
2022-04-26 22:44:33 +0300

    create json-server

--- js/script.js
+++ js/script.js
@@ -279,6 +279,8 @@ window.addEventListener('DOMContentLoaded', function () {
         }, 2000);        
     }
 
-
+    fetch('http://localhost:3000/menu')
+    .then(data=> data.json())
+    .then(res => console.log(res));
 
 });
